ecdc2216e390751f40ec15c5c23cdbd40c2ba2e1
justincdavis <jcdavis@mssm.org>
2022-09-23 09:24:01 -0600

    the commit message for your changes. Lines starting

--- engine/configuration/config.py
+++ engine/configuration/config.py
@@ -5,8 +5,8 @@ class Config:
     """
     Class for loading configuration files and editing their values dynamically at runtime.
     """
+
     def __init__(self, config_file: str):
         assert config_file.endswith(".json") or config_file.endswith(".ini")
 
         self._filename = config_file
-
