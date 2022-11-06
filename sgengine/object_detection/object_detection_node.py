63196583cb91093d7775f1803522d479d10fc44b
ccsmith03 <cailynclaires@gmail.com>
2022-11-06 23:43:43 +0000

    Added multiple subscribers to object detection

--- sgengine/object_detection/object_detection_node.py
+++ sgengine/object_detection/object_detection_node.py
@@ -7,21 +7,34 @@ class ObjectDetectionNode(AbstractNode):
     """Node for handling object detection"""
     def __init__(self, name: str, *args, **kwargs) -> None:
         super().__init__(name, *args, **kwargs)
-        self._sensor_data: SensorData = None
+        self.color_image: Optional[ColorImage] = None
+        self.depth_image: Optional[DepthImage] = None
+        self.imu_data: Optional[ImuData] = None
         self._objects: List[Any] = []
-        self._object_publisher: Optional[Publisher] = None
-        self._sensor_subscriber: Optional[Subscription] = None
 
     def _main(self):
         self.publish("objects", [])
         self.subscribe(
-            "sensor data", self._sensor_callback, SensorData
+            "color image", self._color_callback, ColorImage
+        )
+        self.subscribe(
+            "depth image", self._depth_callback, DepthImage
+        )
+        self.subscribe(
+            "imu data", self._imu_callback, ImuData
         )
 
-    def _sensor_callback(self, msg: SensorData):
+    def _color_callback(self, msg: ColorImage):
         self._sensor_data = msg
         self._find_objects()
-        pass
+
+    def _depth_callback(self, msg: DepthImage):
+        self._sensor_data = msg
+        self._find_objects()
+    
+    def _imu_callback(self, msg: ImuData):
+        self._sensor_data = msg
+        self._find_objects()
 
     @abstractmethod
     def _find_objects():
