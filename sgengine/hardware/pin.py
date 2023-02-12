fcc3707525cfc6fdc6fbc450e1c1ea5839c05f5e
mmcfeeters <mmcfeeters@mines.edu>
2023-02-12 16:46:06 -0700

    Updated pin class so pin is set up through RPi.GPIO on creation

--- sgengine/hardware/pin.py
+++ sgengine/hardware/pin.py
@@ -1,28 +1,38 @@
+#import RPi.GPIO as GPIO
+
 class Pin:
-    def __init__(ID, PinMode, analog_or_dig):
-        self.ID             #int representing the pin. Intrinsic definition of the pin (essentially marks its location)
-        self.pin_mode       #string
-        self.analog_or_dig  #string (either analog or dig) #TODO find better system, int? Enumerated type?
+    def __init__(pin_num, io_mode, is_dig):
+        self.pin_num             #int representing the pin. Intrinsic definition of the pin (essentially marks its location)
+        self.io_mode             #boolean for whether the pin is an input or output pin. True if input, false if output
+        self.is_dig              #boolean for whether the pin is analog or digital
+        
+        #setup the pin
+        if(io_mode):
+            GPIO.setup(pin_num, GPIO.IN)
+        else:
+            GPIO.setup(pin_num, GPIO.OUT)
 
     def __str__(self):
-        return f"Pin ID: {ID}"
+        return f"Pin number: {pin_num}"
 
     #getters and setters
-    def set_ID(self, new_ID):
-        self.ID = new_ID
+    def set_pin_num(self, new_num):
+        self.pin_num = new_num
 
-    def get_ID(self):
-        return ID 
+    def get_pin_num(self):
+        return pin_num 
     
-    def set_mode(self, new_Pin_Mode):
-        self.pin_mode = new_Pin_Mode
+    def set_input(self, is_input):
+        self.io_mode = is_input
+        #reset the GPIO pin
+        GPIO.setup(self.pin_num, is_input)
 
-    def get_mode(self):
-        return pin_mode
+    def get_input(self):
+        return is_input
 
-    def set_analog_or_dig(self, is_analog_or_dig):
-        self.analog_or_dig = is_analog_or_dig
+    def set_is_dig(self, set_to_dig):
+        self.is_dig = set_to_dig
 
-    def get_analog_or_dig(self):
-        return analog_or_dig
+    def get_is_dig(self):
+        return is_dig
 
